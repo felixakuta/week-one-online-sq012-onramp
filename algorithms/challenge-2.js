c27d4a4dff78d6cf0183c9b780c073f1cfdcad04
unknown <felixezeugo@gmail.com>
2022-06-22 22:25:03 +0100

    second challenge update

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,5 +1,8 @@
 function filterArray(arr) {
- 
+    return arr.filter((elem)=>{return typeof elem==='number'});
+
 }   
 
 module.exports = filterArray
+
+console.log(filterArray([1,2,3,4,"me","8"]));
--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,5 +1,21 @@
 function sumMix(arr) {
-   
+    let sum=0;
+    arr.forEach(item => {
+        if(typeof item==="string"){
+            item=Number(item);
+            sum+=item;
+        }
+        else{
+            sum+=item;
+        
+     }
+        
+    });
+    return sum;
+    
  }
 
-  module.exports = sumMix
+     module.exports = sumMix
+
+     console.log(sumMix([23,1,5,"1","6"]));
+
